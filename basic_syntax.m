5b6fd1658fd00be9aaa73240e9da255f7ba2259a
subrata das <sd7184732@gmail.com>
2025-06-29 23:42:37 +0600

    Add files via upload

---
+++ basic_syntax.m
@@ -0,0 +1,7 @@
+A=[1,2,3,4;5,6,7,8;9,10,11,12;13,14,15,16];
+A %that print 4 by 4 matrix.
+size(A) %4 4
+A(2,4) %represents index.ans = 8
+A(2:3,2:3) %index of (2nd & 3rd row - 2nd column & 3rd column)
+w = 1:10 %print 1 to 10
+size(w) % 1 10
