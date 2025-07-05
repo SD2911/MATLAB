602e8db5e813c65df430c9ac261466bf84a85dc0
subrata das <sd7184732@gmail.com>
2025-07-05 18:46:09 +0600

    Add files via upload

---
+++ datatypes.m
@@ -0,0 +1,11 @@
+my_struct.name = 'subrata das'
+my_struct.age = 22
+setfield(my_struct,'gender','male')
+my_struct.contact.phone = 01764035252
+my_struct.contact.email='person@gmail.com'
+
+s=struct('name','subrata','email','hydrogenbd82@gmail.com')
+
+
+
+
